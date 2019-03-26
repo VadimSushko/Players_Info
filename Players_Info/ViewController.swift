53a5e366f31e90a93d6c3d4959bd88076f699629
Vadim Sushko <vadim.sushko@gmail.com>
2019-03-26 21:53:08 +0200

    Initial Commit

---
+++ Players_Info/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Players_Info
+//
+//  Created by Vadym Sushko on 3/26/19.
+//  Copyright © 2019 Vadym Sushko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
